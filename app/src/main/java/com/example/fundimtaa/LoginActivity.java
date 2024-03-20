e4f4bd1b1a54b00695fe822b5f0495d14d30e9dc
John Mutua <mutuabenedict920@gmail.com>
2024-03-20 18:14:19 +0300

    Made some changes

--- app/src/main/java/com/example/fundimtaa/LoginActivity.java
+++ app/src/main/java/com/example/fundimtaa/LoginActivity.java
@@ -14,8 +14,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -29,7 +29,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private GoogleSignInOptions gso;
-    private GoogleApiClient mGoogleApiClient; // Google Sign-In client
+    private GoogleSignInClient mGoogleSignInClient; // Google Sign-In client
     private static final int RC_SIGN_IN = 9001; // Request code for Google Sign-In
 
     @Override
@@ -41,13 +41,11 @@ public class LoginActivity extends AppCompatActivity {
 
         // Configure Google Sign-In
         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.app_name)) // Use default web client ID
+                .requestIdToken(getString(R.string.default_web_client_id)) // Use your actual Web Client ID here
                 .requestEmail()
                 .build();
 
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
         // Find the Sign Up TextView in LoginActivity
         TextView textViewRegister = findViewById(R.id.textViewRegister);
@@ -115,7 +113,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void signInWithGoogle() {
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
