bd4ec61780102072a9a98f82cee886c75fb2ba9f
John Mutua <mutuabenedict920@gmail.com>
2024-04-05 19:30:19 +0300

    Worked partially on my posting job interface

--- app/src/main/java/com/example/fundimtaa/ClientHomeDashboardActivity.java
+++ app/src/main/java/com/example/fundimtaa/ClientHomeDashboardActivity.java
@@ -1,8 +1,14 @@
 package com.example.fundimtaa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ClientHomeDashboardActivity extends AppCompatActivity {
 
@@ -10,5 +16,27 @@ public class ClientHomeDashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_home_dashboard);
+
+        // Find views
+        CardView postJobCard = findViewById(R.id.card_post_job);
+
+        FloatingActionButton floatingActionButton = findViewById(R.id.floatingButton);
+        // Set OnClickListener for the post job card
+        postJobCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to the job posting interface
+                startActivity(new Intent(ClientHomeDashboardActivity.this, PostingActivity.class));
+            }
+        });
+
+        // Set OnClickListener for the floating action button
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to the post job interface
+                startActivity(new Intent(ClientHomeDashboardActivity.this, PostJobActivity.class));
+            }
+        });
     }
 }
