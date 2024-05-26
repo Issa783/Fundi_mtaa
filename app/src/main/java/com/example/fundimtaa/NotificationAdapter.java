5283d83bcd04f18d9f4bc1b75ca6fd2d54988648
Issa783 <126448835+Issa783@users.noreply.github.com>
2024-05-26 19:06:06 +0300

    Update NotificationAdapter.java

--- app/src/main/java/com/example/fundimtaa/NotificationAdapter.java
+++ app/src/main/java/com/example/fundimtaa/NotificationAdapter.java
@@ -7,45 +7,64 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
-
 import java.util.List;
 
-public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.ViewHolder> {
-    private List<Notification> notifications;
+public class NotificationAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private static final int TYPE_HEADER = 0;
+    private static final int TYPE_ITEM = 1;
+    private List<Object> notifications;
     private Context context;
 
-    public NotificationAdapter(List<Notification> notifications, Context context) {
+    public NotificationAdapter(List<Object> notifications, Context context) {
         this.notifications = notifications;
         this.context = context;
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_notification, parent, false);
-        return new ViewHolder(view);
+    public int getItemViewType(int position) {
+        if (notifications.get(position) instanceof String) {
+            return TYPE_HEADER;
+        }
+        return TYPE_ITEM;
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        Notification notification = notifications.get(position);
-        holder.titleTextView.setText(notification.getTitle());
-        holder.bodyTextView.setText(notification.getBody());
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == TYPE_HEADER) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_notification_header, parent, false);
+            return new HeaderViewHolder(view);
+        } else {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_notification, parent, false);
+            return new ItemViewHolder(view);
+        }
+    }
 
-        holder.itemView.setOnClickListener(v -> {
-            Intent intent = new Intent(context, NotificationDetailActivity.class);
-            intent.putExtra("notificationTitle", notification.getTitle());
-            intent.putExtra("notificationBody", notification.getBody());
-            context.startActivity(intent);
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof ItemViewHolder) {
+            Notification notification = (Notification) notifications.get(position);
+            ItemViewHolder itemViewHolder = (ItemViewHolder) holder;
+            itemViewHolder.titleTextView.setText(notification.getTitle());
+            itemViewHolder.bodyTextView.setText(notification.getBody());
 
-            // Mark as read
-            markNotificationAsRead(notification.getId());
-        });
+            itemViewHolder.itemView.setOnClickListener(v -> {
+                Intent intent = new Intent(context, NotificationDetailActivity.class);
+                intent.putExtra("notificationTitle", notification.getTitle());
+                intent.putExtra("notificationBody", notification.getBody());
+                context.startActivity(intent);
+
+                markNotificationAsRead(notification.getId());
+            });
+        } else if (holder instanceof HeaderViewHolder) {
+            String headerTitle = (String) notifications.get(position);
+            HeaderViewHolder headerViewHolder = (HeaderViewHolder) holder;
+            headerViewHolder.headerTextView.setText(headerTitle);
+        }
     }
 
     @Override
@@ -53,60 +72,63 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
         return notifications.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ItemViewHolder extends RecyclerView.ViewHolder {
         public TextView titleTextView;
         public TextView bodyTextView;
+        public CardView cardView;
 
-        public ViewHolder(View itemView) {
+        public ItemViewHolder(View itemView) {
             super(itemView);
             titleTextView = itemView.findViewById(R.id.notificationTitle);
             bodyTextView = itemView.findViewById(R.id.notificationBody);
+            cardView = itemView.findViewById(R.id.cardView);
         }
     }
 
-  private void markNotificationAsRead(String notificationId) {
-    FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
-    if (currentUser != null) {
-        String userId = currentUser.getUid();
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
+    public static class HeaderViewHolder extends RecyclerView.ViewHolder {
+        public TextView headerTextView;
 
-        // Check if the user is a client
-        db.collection("clients").document(userId).get().addOnSuccessListener(documentSnapshot -> {
-            if (documentSnapshot.exists()) {
-                // User is a client, update notification in the clients collection
-                db.collection("clients").document(userId).collection("notifications")
-                        .document(notificationId)
-                        .update("read", true)
-                        .addOnSuccessListener(aVoid -> {
-                            // Optionally, update the unread count
-                            if (context instanceof ClientHomeDashboardActivity) {
-                                ((ClientHomeDashboardActivity) context).updateNotificationIndicator();
-                            }
-                        })
-                        .addOnFailureListener(e -> Log.e("NotificationAdapter", "Error marking notification as read: " + e.getMessage()));
-            } else {
-                // User is not a client, check if they are a worker
-                db.collection("workers").document(userId).get().addOnSuccessListener(workerDocumentSnapshot -> {
-                    if (workerDocumentSnapshot.exists()) {
-                        // User is a worker, update notification in the workers collection
-                        db.collection("workers").document(userId).collection("notifications")
-                                .document(notificationId)
-                                .update("read", true)
-                                .addOnSuccessListener(aVoid -> {
-                                    // Optionally, update the unread count
-                                    if (context instanceof WorkerHomeDashboardActivity) {
-                                        ((WorkerHomeDashboardActivity) context).updateNotificationIndicator();
-                                    }
-                                })
-                                .addOnFailureListener(e -> Log.e("NotificationAdapter", "Error marking notification as read: " + e.getMessage()));
-                    } else {
-                        // User role not found
-                        Log.e("NotificationAdapter", "Error marking notification as read: User role not found");
-                    }
-                }).addOnFailureListener(e -> Log.e("NotificationAdapter", "Error checking worker document: " + e.getMessage()));
-            }
-        }).addOnFailureListener(e -> Log.e("NotificationAdapter", "Error checking client document: " + e.getMessage()));
+        public HeaderViewHolder(View itemView) {
+            super(itemView);
+            headerTextView = itemView.findViewById(R.id.notificationHeader);
+        }
     }
-}
 
+    private void markNotificationAsRead(String notificationId) {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser != null) {
+            String userId = currentUser.getUid();
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+            db.collection("clients").document(userId).get().addOnSuccessListener(documentSnapshot -> {
+                if (documentSnapshot.exists()) {
+                    db.collection("clients").document(userId).collection("notifications")
+                            .document(notificationId)
+                            .update("read", true)
+                            .addOnSuccessListener(aVoid -> {
+                                if (context instanceof ClientHomeDashboardActivity) {
+                                    ((ClientHomeDashboardActivity) context).updateNotificationIndicator();
+                                }
+                            })
+                            .addOnFailureListener(e -> Log.e("NotificationAdapter", "Error marking notification as read: " + e.getMessage()));
+                } else {
+                    db.collection("workers").document(userId).get().addOnSuccessListener(workerDocumentSnapshot -> {
+                        if (workerDocumentSnapshot.exists()) {
+                            db.collection("workers").document(userId).collection("notifications")
+                                    .document(notificationId)
+                                    .update("read", true)
+                                    .addOnSuccessListener(aVoid -> {
+                                        if (context instanceof WorkerHomeDashboardActivity) {
+                                            ((WorkerHomeDashboardActivity) context).updateNotificationIndicator();
+                                        }
+                                    })
+                                    .addOnFailureListener(e -> Log.e("NotificationAdapter", "Error marking notification as read: " + e.getMessage()));
+                        } else {
+                            Log.e("NotificationAdapter", "Error marking notification as read: User role not found");
+                        }
+                    }).addOnFailureListener(e -> Log.e("NotificationAdapter", "Error checking worker document: " + e.getMessage()));
+                }
+            }).addOnFailureListener(e -> Log.e("NotificationAdapter", "Error checking client document: " + e.getMessage()));
+        }
+    }
 }
