3bf5434c62ed35aa7d4c843f2273fa8577a3f7df
John Mutua <mutuabenedict920@gmail.com>
2024-05-28 12:07:46 +0300

    added back arrow functionality

--- app/src/main/java/com/example/fundimtaa/ClientJobHistoryActivity.java
+++ app/src/main/java/com/example/fundimtaa/ClientJobHistoryActivity.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -36,6 +37,14 @@ public class ClientJobHistoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_job_history);
+        ImageView imageViewBackArrow = findViewById(R.id.imageViewBackArrow);
+        imageViewBackArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate back to the previous activity
+                onBackPressed();
+            }
+        });
 
         // Initialize RecyclerView
         recyclerViewClientJobHistory = findViewById(R.id.recyclerClientJobHistory);
